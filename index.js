035d6daa7ab051444c1e6f2917056964f700a833
Dorker <142069939+meowgoober@users.noreply.github.com>
2025-02-27 02:07:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://mathsspot.com/apps/uncube/10005/now.html/';
+const nggUrl = 'https://mathsspot.com/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
